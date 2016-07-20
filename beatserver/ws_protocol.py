86b91befbfcd0f6a905729c694234aa71a7ec017
Raja Simon <rajasimon@icloud.com>
2016-07-20 20:11:51 +0530

    callback support added

--- beatserver/ws_protocol.py
+++ beatserver/ws_protocol.py
@@ -1,8 +1,9 @@
 import json
 import logging
+from server import reactor
+from twisted.internet.protocol import ReconnectingClientFactory
 from autobahn.twisted.websocket import WebSocketClientFactory, \
     WebSocketClientProtocol
-
 
 logger = logging.getLogger(__name__)
 
@@ -12,31 +13,34 @@ class SocketClientProtocol(WebSocketClientProtocol):
     want to listen only on the websockets now
     """
 
+    def sendBack(self, payload):
+        # find the correct way to identify the connection dropped
+        if self.wasNotCleanReason:
+            reactor.stop()
+        delay = payload.get("delay")
+        number = payload.get("number")
+        self.sendMessage(
+            "number {} every {}!".format(
+                number, delay).encode('utf8'))
+        reactor.callLater(delay, self.sendBack, payload=payload)
 
     def onMessage(self, payload, isBinary):
+        # else websocket doesn't have the status and skip
         payload = json.loads(payload)
-        status = payload['status']
-        if status:
-            self.sendMessage("Hello, world!")
-        # if isBinary:
-        #     print("Binary message received: {0} bytes".format(len(payload)))
-        # else:
-        #     print("Text message received: {0}".format(payload.decode('utf8')))
+        if payload.get("status"):
+            self.sendBack(payload)
 
 
-class SocketClientFactory(WebSocketClientFactory):
+class SocketClientFactory(WebSocketClientFactory, ReconnectingClientFactory):
     protocol = SocketClientProtocol
 
-    # startedConnecting(self, connector): want this ?
+    def startedConnecting(self, connector):
+        print("connected to the client")
 
     def clientConnectionFailed(self, connector, reason):
-        # websocket server failed to connect
-        # just reconnect unitl websocket available ( no print because in loop )
-        # connector.connect()
-        logging.warning("websocket server failed to connect, please restart")
+        print("Client connection failed .. retrying ..")
+        self.retry(connector)
 
     def clientConnectionLost(self, connector, reason):
-        # websocket connection disconnected
-        # just reconnect unitl websocket available ( no print because in loop )
-        # connector.connect()
-        logging.warning("websocket connection disconnected, please restart")
+        print("Client connection lost .. retrying ..")
+        self.retry(connector)
