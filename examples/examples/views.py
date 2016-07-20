af3f4472e80b2aa82d499919885ef2f521b9ecd8
Raja Simon <rajasimon@icloud.com>
2016-07-20 20:10:03 +0530

    clean code and updated to match consumers

--- examples/examples/views.py
+++ examples/examples/views.py
@@ -1,10 +1,16 @@
-from django.shortcuts import render
-from django.http import HttpResponse
-from channels import Channel
-
 from random import randint
+from channels import Channel
+from django.http import HttpResponse
+
+
 # Create your views here.
 def home(request):
-    number =  randint(1, 343333333)
-    Channel('hae-repeat-me').send({'number': number, 'status': True})
+    number = randint(1, 343333333)
+    Channel('hae-repeat-me').send(
+        {
+            'number': number,
+            'status': True,
+            'delay': 10
+        }
+    )
     return HttpResponse("testing")
