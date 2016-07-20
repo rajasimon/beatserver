dc7cc7a3bc296b69222182e5c1c47ec006f27d55
Raja Simon <rajasimon@icloud.com>
2016-07-20 20:10:31 +0530

    added delay params

--- examples/examples/consumers.py
+++ examples/examples/consumers.py
@@ -3,19 +3,16 @@ import json
 # In consumers.py
 from channels import Group
 
+
 # Connected to websocket.connect
 def ws_add(message):
     Group("chat").add(message.reply_channel)
 
+
 # Connected to websocket.receive
 def ws_message(message):
-    print "Receive Message now"
-    # Group("chat").send({
-    #     "text": "[user] %s" % message.content['text'],
-    # })
-    Group("chat").send({
-        "text": json.dumps({'status': False})
-    })
+    print message.content
+
 
 # Connected to websocket.disconnect
 def ws_disconnect(message):
@@ -24,6 +21,10 @@ def ws_disconnect(message):
 
 def hea_repeat_me(message):
     Group("chat").send(
-    {
-        "text": json.dumps({'status': message.content['status'], 'number': message.content['number']})
-    })
+        {
+            "text": json.dumps(
+                {
+                    'status': message.content['status'],
+                    'delay': message.content['delay'],
+                    'number': message.content['number']})
+        })
