bf8c3386284f6c6a11d4122d4e239d61debcd58d
Raja Simon <rajasimon@icloud.com>
2016-07-30 22:04:07 +0530

    removed port and host in test cli

--- beatserver/tests/test_cli.py
+++ beatserver/tests/test_cli.py
@@ -5,20 +5,6 @@ import argparse
 def create_parser():
     parser = argparse.ArgumentParser()
     parser.add_argument(
-        '-p',
-        '--port',
-        type=int,
-        help='Port number to listen on',
-        default=8000,
-    )
-    parser.add_argument(
-        '-b',
-        '--bind',
-        dest='host',
-        help='The host/address to bind to',
-        default="127.0.0.1",
-    )
-    parser.add_argument(
         'channel_layer',
         help='ASGI channel layer')
     return parser
@@ -31,8 +17,6 @@ class TestCli(unittest.TestCase):
     def test_no_args(self):
         # test without arguments
         parsed = self.parser.parse_args(['examples.asgi:channel_layer'])
-        self.assertEqual(parsed.host, '127.0.0.1')
-        self.assertEqual(parsed.port, 8000)
         self.assertEqual(parsed.channel_layer, "examples.asgi:channel_layer")
 
 
