c38c3f30f48973c78eda6a97d1bd8336a2ffbe21
Raja Simon <rajasimon@icloud.com>
2016-07-30 21:33:59 +0530

    single class file and improved code for upcoming testcase

--- beatserver/parser.py
+++ beatserver/parser.py
@@ -2,29 +2,26 @@ import importlib
 from channels import channel_layers
 
 
-def cli_parser(args):
-    module_path, object_path = args.channel_layer.split(":", 1)
-    channel_layer = importlib.import_module(module_path)
-    project_name, _ = module_path.split('.')
-    return channel_layer, project_name
-
-
 class Parser(object):
+    """
+    Takes module_path as arguments path.module and also it has the function to 
+    check the in-memory layer.
+    """
 
-    file_name = 'beatconfig'
-
-    def __init__(self, project_name=None):
-        self.project_name = project_name
+    def __init__(self, module_path):
+        self.module_path = module_path
 
     def check_in_memory(self):
-        backend = channel_layers.config['defaut']['BACKEND']
+        backend = channel_layers.configs['default']['BACKEND']
         if backend == "asgiref.inmemory.ChannelLayer":
             raise Exception(
                 " beatserver will not support inmemory Channel layer")
 
     def get_module(self):
-        return importlib.import_module(
-            '{}.{}'.format(self.project_name, self.file_name))
+        return importlib.import_module(self.module_path)
 
-    def get_tasks(self):
+    def get_channel_layer(self):
+        return self.get_module()
+
+    def get_beat_config(self):
         return self.get_module().BEAT_SCHEDULE
