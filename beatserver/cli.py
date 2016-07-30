5364912f8cfb5f7f90b5d4abfc79d1df13a2e64e
Raja Simon <rajasimon@icloud.com>
2016-07-30 21:35:12 +0530

    removed host and port no need  and improve code

--- beatserver/cli.py
+++ beatserver/cli.py
@@ -2,7 +2,7 @@ import sys
 import argparse
 import logging
 from .server import Server
-from .parser import cli_parser
+from .parser import Parser
 
 logger = logging.getLogger(__name__)
 logging.basicConfig(format='%(levelname)s:%(message)s', level=logging.INFO)
@@ -10,7 +10,7 @@ logging.basicConfig(format='%(levelname)s:%(message)s', level=logging.INFO)
 
 class CommandLineInterface(object):
     """
-    Acts as the main enrtypoint for running the server
+    Acts as the main enrtypoint for running the server...
     """
 
     description = "Beat Server"
@@ -18,20 +18,6 @@ class CommandLineInterface(object):
     def __init__(self):
         self.parser = argparse.ArgumentParser(description=self.description)
 
-        self.parser.add_argument(
-            '-p',
-            '--port',
-            type=int,
-            help='Port number to listen on',
-            default=8000,
-        )
-        self.parser.add_argument(
-            '-b',
-            '--bind',
-            dest='host',
-            help='The host/address to bind to',
-            default="127.0.0.1",
-        )
         self.parser.add_argument(
             'channel_layer',
             help='ASGI channel layer')
@@ -53,11 +39,17 @@ class CommandLineInterface(object):
 
         # Import channel layer
         sys.path.insert(0, ".")
-        channel_layer, project_name = cli_parser(args)
 
+        # get the modlue path from command line args
+        module_path, object_path = args.channel_layer.split(":", 1)
+
+        # get channel_layer, beatconfig from parser object
+        channel_layer = Parser(module_path).get_channel_layer()
+        beat_config = Parser(
+            module_path.split('.')[0] + ".beatconfig").get_beat_config() 
+
+        Parser(module_path).check_in_memory()
         # call the Server with args and run method
         Server(
-            port=args.port,
-            host=args.host,
-            project_name=project_name,
-            channel_layer=channel_layer).run()
+            channel_layer=channel_layer,
+            beat_config=beat_config).run()
