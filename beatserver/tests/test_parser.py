bd0493db0324a22ba2f748a328d9f9824a29364d
Raja Simon <rajasimon@icloud.com>
2016-07-31 21:32:42 +0530

    changed testcase for parser a bit to match test_beatconfig file - that was added

--- beatserver/tests/test_parser.py
+++ beatserver/tests/test_parser.py
@@ -1,24 +1,21 @@
+import sys
+import importlib
 import unittest
 from datetime import timedelta
 
-
+ 
 class TestParser(unittest.TestCase):
 
-    BEAT_SCHEDULE = {
-        'add-every-10-seconds': {
-            'channel_name': 'testing',
-            'schedule': timedelta(seconds=30),
-            'message': {'status': True, 'test': True}
-        },
+    def setUp(self):
+        sys.path.insert(0, '.')
+        self.get_module = importlib.import_module('test_beatconfig')
+
+    beat = {
+        'channel_name': 'testing',
+        'schedule': timedelta(seconds=10),
+        'message': {'foo': 'bar'}
     }
-
-    def get_beat(self):
-        return self.BEAT_SCHEDULE['add-every-10-seconds']
-
-    def test_tasks(self):
-        configs = {
-            'channel_name': 'testing',
-            'schedule': timedelta(seconds=30),
-            'message': {'status': True, 'test': True}
-        }
-        self.assertEqual(self.get_beat(), configs)
+    
+    def test_beat_config(self):
+        self.assertEqual(
+            self.get_module.BEAT_SCHEDULE['add-every-10-seconds'], self.beat)
