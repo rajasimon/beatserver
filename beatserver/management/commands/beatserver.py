30f773e3c4c0bce981e3ed4083aef2cc4bad7478
creativequality <informatica@creative-quality.es>
2019-01-24 17:55:34 +0100

    Fix bug channels 2.1.4
    Fix bug to work with Django Channels 2.1.4 and later

--- beatserver/management/commands/beatserver.py
+++ beatserver/management/commands/beatserver.py
@@ -1,15 +1,15 @@
 import os
+import logging
 from django.core.management import BaseCommand, CommandError
 from django.conf import settings
 
 from channels import DEFAULT_CHANNEL_LAYER
 from channels.layers import get_channel_layer
-from channels.log import setup_logger
 from channels.routing import get_default_application
 
 from beatserver.server import BeatServer
 
-
+logger = logging.getLogger("django.channels.worker")
 
 class Command(BaseCommand):
 
@@ -24,8 +24,6 @@ class Command(BaseCommand):
         )
 
     def handle(self, *args, **options):
-        # Get the backend to use
-        self.verbosity = options.get("verbosity", 1)
         # Get the channel layer they asked for (or see if one isn't configured)
         if "layer" in options:
             self.channel_layer = get_channel_layer(options["layer"])
@@ -33,15 +31,14 @@ class Command(BaseCommand):
             self.channel_layer = get_channel_layer()
         if self.channel_layer is None:
             raise CommandError("You do not have any CHANNEL_LAYERS configured.")
-        
+
         # Get beatconfig here.
         from beatserver.parser import Parser
         project_path = settings.SETTINGS_MODULE.replace('.settings', '')
         beat_config = Parser(project_path + '.beatconfig').get_beat_config()
 
         # Run the worker
-        self.logger = setup_logger("django.channels", self.verbosity)
-        self.logger.info("Starting beatserver...")
+        logger.info("Starting beatserver...")
         server = self.server_class(
             application=get_default_application(),
             channel_layer=self.channel_layer,
