af5be7fe91f11353ed3fcc76a90df4c077786f1d
Raja Simon <rajasimonio@gmail.com>
2022-08-02 17:51:57 +0530

    Fix readme not reflect in PyPi description

--- setup.py
+++ setup.py
@@ -2,16 +2,25 @@ from setuptools import setup, find_packages
 from beatserver import __version__
 
 
+def get_long_description():
+    """
+    Return the README.
+    """
+    with open("README.md", encoding="utf8") as f:
+        return f.read()
+
+
 setup(
     name="beatserver",
     version=__version__,
     url="https://github.com/rajasimon/beatserver",
-    author='Raja Simon',
-    author_email='rajasimon@icloud.com',
+    author="Raja Simon",
+    author_email="rajasimon@icloud.com",
     description="Beat Server is is a scheduler",
-    license='MIT',
+    long_description=get_long_description(),
+    long_description_content_type="text/markdown",
+    license="MIT",
     packages=find_packages(),
-    install_requires=['channels>=1.1.8.1', 'croniter>=0.3.30'],
-    include_package_data=True
-
+    install_requires=["channels>=1.1.8.1", "croniter>=0.3.30"],
+    include_package_data=True,
 )
