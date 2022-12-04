90096da32739a912baf03f3e1f433a61a88c2a4a
Raja Simon <rajasimonio@gmail.com>
2022-12-05 00:30:15 +0530

    Fix wrong runserver

--- examples/examples/settings.py
+++ examples/examples/settings.py
@@ -31,14 +31,14 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    "daphne",
+    "beatserver",
     "django.contrib.admin",
     "django.contrib.auth",
     "django.contrib.contenttypes",
     "django.contrib.sessions",
     "django.contrib.messages",
     "django.contrib.staticfiles",
-    "channels",
-    "beatserver",
 ]
 
 MIDDLEWARE = [
